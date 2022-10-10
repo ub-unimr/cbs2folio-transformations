5e23685728cfc6a3e28fc5c281513c89df836f1f
Paul Münch <paul.muench@ub.uni-marburg.de>
2022-10-10 14:06:06 +0200

    Use code instead of name to convert locations.

--- hebis/tools/scripts/convert_json_to_xsl.py
+++ hebis/tools/scripts/convert_json_to_xsl.py
@@ -5,4 +5,4 @@ import sys
 with open(sys.argv[1]) as file:
     data = json.load(file)
     for i in data[list(data.keys())[0]]:
-        print("<xsl:when test=\".='" + i['name'] + "'\">" + i['id'] + "</xsl:when>")
+        print("<xsl:when test=\".='" + i['code'] + "'\">" + i['id'] + "</xsl:when>")
